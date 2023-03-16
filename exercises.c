cc988b86e61dd260990e0d666af7304065e11cda
Replit user <>
2023-03-16 16:10:02 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -28,7 +28,7 @@ almacena en las variables apuntadas por max y min.
 void arrayMaxMin(int *a, int n, int *max, int *min) {
   int i;
   *max=a[0];
-  *max=a[0];
+  *min=a[0];
   for (i=1;i<n;i++ )
     {
       if (a[i]> *max)
